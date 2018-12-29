60dd72f6e4ac2e6180bdc77a6e0d98cc8c242586
David Asabina <vid@bina.me>
2018-12-29 03:01:17 +0100

    Adding note on libxcb problem

--- default.nix
+++ default.nix
@@ -1,5 +1,11 @@
 { stdenv, pkgs }:
 
+# Run as non-pure shell by just invoking `nix-shell`  instead of
+# `nix-shell --pure` since there are some Qt dependencies that can cause
+# trouble i.e.: libxcb can't be found in pure mode and if QT_QPA_PLATFORM_PATH
+# has been defined to point towards a path that contains the libxcb lib, a
+# QXcbIntegration error is thrown stating "Cannot create platform offscreen
+# surface, neither GLX nor EGL are enabled"
 stdenv.mkDerivation rec {
   name = "ice-setup";
   src = ./.;
